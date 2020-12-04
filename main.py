ee900aacabc1c417999487b383ffc2f37609007b
MrCasco <ernesto.casco.velazquez@hotmail.com>
2020-12-03 21:24:01 -0600

    Eval vulnerability worked

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from math import sqrt
 from flask import Flask, render_template, request
+import unicodedata
 
 app = Flask(__name__)
+
+def calculate(a, b, c):
+    result = lambda op: eval("(1/(2*%s))*(%s %s %s)" % (a, -float(b), op, getSqrt(b, a, c)))
+    print(result("-"))
+    return [result("-"), result("+")]
+
+def getSqrt(b, a, c):
+    try:
+        return sqrt(b*b - 4*a*c)
+    except:
+        return "Not"
 
 @app.route('/')
 def index():
@@ -11,13 +23,12 @@ def index():
         print("Error: ", e)
         return "Hello world"
 
-@app.route('/hello', methods=['POST'])
+@app.route('/params', methods=['POST'])
 def params():
-    a = request.form['a']
-    b = request.form['b']
-    c = request.form['c']
-    sq = sqrt(b*b - 4*a*c)
-    return (a,b,c)
+    a = str(request.form['a']).strip('u').strip('\'')
+    b = str(request.form['b']).strip('u').strip('\'')
+    c = str(request.form['c']).strip('u').strip('\'')
+    return "Your result is: "+str(calculate(a, b, c)[0])+", "+str(calculate(a, b, c)[1])
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
@@ -31,9 +42,4 @@ print(a, b, c)
 sq = sqrt(b*b - 4*a*c)
 print(eval("(%s - %s)/(2*%s)" % (-b, sq, a)))
 
-def calculate(a, b, c):
-    sq = sqrt(b*b - 4*a*c)
-    result = lambda op: eval("(%s %s %s)/(2*%s)" % (-b, op, sq, a))
-    return [result("-"), result("+")]
-
-print("Your result is: "+str(calculate(a, b, c)[0])+", "+str(calculate(a, b, c)[1]))
+#print("Your result is: "+str(calculate(a, b, c)[0])+", "+str(calculate(a, b, c)[1]))
